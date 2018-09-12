78fa3b8d60619568104f7caecf405486dcde476a
TayHays <39424304+TayHays@users.noreply.github.com>
2018-09-12 12:07:15 -0500

    Peer Code Push
    lots done

---
+++ js/functions.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+
+
+		$('.main-gallery').flickity({
+		  // options
+		  cellAlign: 'left',
+		  contain: true
+		});
+
+
+        $( function()
+        {
+            var targets = $( '[rel~=tooltip]' ),
+                target  = false,
+                tooltip = false,
+                title   = false;
+			
+            targets.bind( 'mouseenter', function()
+            {
+                target  = $( this );
+                tip     = target.attr( 'title' );
+                tooltip = $( '<div id="tooltip"></div>' );
+                if( !tip || tip == '' )
+                    return false;
+                target.removeAttr( 'title' );
+                tooltip.css( 'opacity', 0 )
+                       .html( tip )
+                       .appendTo( 'body' );
+				
+                var init_tooltip = function()
+                {
+                    if( $( window ).width() < tooltip.outerWidth() * 1.5 )
+                        tooltip.css( 'max-width', $( window ).width() / 2 );
+                    else
+                        tooltip.css( 'max-width', 340 );
+                    var pos_left = target.offset().left + ( target.outerWidth() ) + 20,
+                        pos_top  = target.offset().top - tooltip.outerHeight() + 30;
+    
+                    
+                    tooltip.css( { left: pos_left, top: pos_top } )
+                           .animate( { top: '+=10', opacity: 1 }, 50 );
+                };
+                init_tooltip();
+                $( window ).resize( init_tooltip );
+                var remove_tooltip = function()
+                {
+                    tooltip.animate( { top: '-=10', opacity: 0 }, 50, function()
+                    {
+                        $( this ).remove();
+                    });
+                    target.attr( 'title', tip );
+                };
+				
+                target.bind( 'mouseleave', remove_tooltip );
+                tooltip.bind( 'click', remove_tooltip );
+            });
+        });
